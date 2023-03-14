89469b4c0a19dcd4f41d8b2d3954427ec8b8c3a2
Joshua030608 <64506906+Joshua030608@users.noreply.github.com>
2023-03-14 19:09:09 -0400

    Initial Commit

---
+++ CardViewer/CardViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardViewerApp.swift
+//  CardViewer
+//
+//  Created by Joshua Ford on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct CardViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
