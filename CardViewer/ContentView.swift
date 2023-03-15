12d37b444c6e340ed36bf76457d34a3c8b8cff81
Joshua030608 <64506906+Joshua030608@users.noreply.github.com>
2023-03-14 20:29:41 -0400

    End of 3.14.23: Start Screen
    Began creating start screen.

--- CardViewer/ContentView.swift
+++ CardViewer/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.red
+                .ignoresSafeArea()
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                Text("CardViewer")
+                Button {
+                    print("Start Pressed")
+                } label: {
+                    Color.green
+                }
+
+            }
         }
-        .padding()
     }
 }
 
