8ba82047989ce36e29871e15f896cf28da8284d7
Joshua030608 <64506906+Joshua030608@users.noreply.github.com>
2023-03-31 15:04:17 -0400

    Homework 3.23.23: Added cardView in list.
    Added Card struct with card properties. Added cardView struct that takes a card and lays out the information. Added dummyData (cards) and then put that in list. List goes through card in Cards and creates a cardView with said card.

--- CardViewer/CollectionView.swift
+++ CardViewer/CollectionView.swift
@@ -7,17 +7,57 @@
 
 import SwiftUI
 
+struct Card: Identifiable {
+    var id = UUID()
+    var playerName: String
+    var team: String
+    var image: Image
+    var position: String
+}
+
+struct CardView: View {
+    var card: Card
+    
+    var body: some View {
+        HStack {
+            VStack {
+                Text(card.playerName)
+                Text(card.team)
+            }
+            Spacer()
+            Text(card.position)
+            Spacer()
+            card.image
+                .resizable()
+                .frame(width: 150, height: 200)
+                .aspectRatio(contentMode: .fit)
+        }
+    }
+}
+
 struct CollectionView: View {
+    
+    let cards = [
+        Card(playerName: "Jalen Hurts", team: "Eagles", image: Image("jalenHurts"), position: "QB"),
+    Card(playerName: "A.J. Brown", team: "Eagles", image: Image("ajBrown"), position: "WR"),
+        Card(playerName: "Boston Scott", team: "Eagles", image: Image("bostonScott"), position: "RB")
+    ]
+    
     var body: some View {
 //        List {
 //            Text("hi")
 //            Image(systemName: "xbox.logo")
 //        }
-        ScrollView(.horizontal) {
-            HStack {
-                ForEach(1..<101) { index in
-                    Text("\(index)")
-                }
+//        ScrollView(.vertical) {
+//            VStack {
+//                ForEach(1..<101) { index in
+//                    Text("\(index)")
+//                }
+//            }
+//        }
+        List {
+            ForEach(cards) { card in
+                CardView(card: card)
             }
         }
     }
