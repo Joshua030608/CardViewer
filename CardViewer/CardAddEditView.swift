c2b081abf6699cb28b5c6d951c96963ee2f300cc
Joshua030608 <64506906+Joshua030608@users.noreply.github.com>
2023-04-03 20:37:09 -0400

    Homework 3.31.23: Continued working on CardAddEdit
    Continued working on CardAddEditView. Added photospicker, textFields for all of the parameters, and picker for grade.

--- CardViewer/CardAddEditView.swift
+++ CardViewer/CardAddEditView.swift
@@ -6,19 +6,80 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct CardAddEditView: View {
     @State private var nameString: String = ""
-    
-    
+    @State private var posititionString: String = ""
+    @State private var teamString: String = ""
+    @State private var grade: String = ""
+    @State private var selectedPhotos: [PhotosPickerItem] = []
+    @State private var data: Data?
     
     
     var body: some View {
         VStack {
-            Image(systemName: "xbox.logo")
-                .padding()
-            TextField("Name", text: .constant(nameString))
-            Text(nameString)
+            PhotosPicker(
+                selection: $selectedPhotos,
+                maxSelectionCount: 5,
+                matching: .images) {
+                    VStack {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                        Text("Add Image(s)")
+                            .font(.largeTitle)
+                    }
+                }.padding(100)
+                .onChange(of: selectedPhotos) { newValue in
+                    guard let item = selectedPhotos.first else {
+                        return
+                    }
+                    item.loadTransferable(type: Data.self) { result in
+                        switch result {
+                        case .success(let data):
+                            if let data = data {
+                                self.data = data
+                            } else {
+                                print("failed to load")
+                            }
+                        case .failure(let failure):
+                            fatalError("\(failure)")
+                        }
+                    }
+                }
+            if let data = data, let uiImage = UIImage(data: data) {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .frame(width: 100, height: 100)
+            }
+            Spacer()
+            TextField("Name", text: $nameString)
+                .multilineTextAlignment(.center)
+                .font(.largeTitle)
+                .textFieldStyle(.roundedBorder)
+            TextField("Team", text: $teamString)
+                .multilineTextAlignment(.center)
+                .font(.title2)
+                .textFieldStyle(.roundedBorder)
+            TextField("Position", text: $posititionString)
+                .multilineTextAlignment(.center)
+                .font(.title2)
+                .textFieldStyle(.roundedBorder)
+            HStack {
+                Text("Grade:")
+                    .font(.title2)
+                Picker(
+                    selection: $grade,
+                    label: Text("1")) {
+                        ForEach(0..<11) { number in
+                            Text("\(number)")
+                                .tag("\(number)")
+                        }
+                }
+            }
+            Text(grade)
+                .padding(100)
         }
     }
 }
